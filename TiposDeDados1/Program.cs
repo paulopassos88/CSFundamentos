fcd64f864ff3a7ecd290b7e36c7b9f0e1e63549d
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-20 19:57:28 -0300

    Add project files.

---
+++ TiposDeDados1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Tipo de dados numericos");
+Console.WriteLine();
+
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = 2147483647;
+uint valor4 = 2147483689;
+long valor5 = -2147483647;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+
+Console.ReadKey();
