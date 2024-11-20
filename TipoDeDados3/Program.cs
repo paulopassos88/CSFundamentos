e9588b3fb7b5af70a632766235c55275ad02a580
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-20 20:34:47 -0300

    Tipo de dados bool e char

---
+++ TipoDeDados3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Tipo de dados bool e char");
+Console.WriteLine();
+
+bool ativo = false;
+System.Boolean inativo = true;
+
+Console.WriteLine(ativo);
+Console.WriteLine(inativo);
+
+Console.WriteLine(10 == 15);
+Console.WriteLine(10 != 15);
+Console.WriteLine(10 == 10);
+
+int x = 10;
+int y = 15;
+
+Console.WriteLine(x > y);
+
+char letra1 = 'A';
+char letra2 = '\u0041';
+
+Console.WriteLine(letra1);
+Console.WriteLine(letra2);
+
+Console.ReadKey();
+
