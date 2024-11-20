52e324d02bc591b3d966663929c848828e284689
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-20 20:10:38 -0300

    Tipo de dado double, float e decimal

---
+++ TipoDeDado2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Tipo de dado double, float e decimal");
+Console.WriteLine();
+
+double valor1 = 1.234;
+float valor2 = 1.234f;
+decimal valor3 = 1.234m;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+
+Console.ReadKey();
