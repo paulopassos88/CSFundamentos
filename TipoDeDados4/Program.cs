4bd83d0dc5f2b2ac4ee0b7b8b25730a2dbb6d844
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-20 20:50:10 -0300

    Tipo de dados string, object e dynamic

---
+++ TipoDeDados4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Tipo de dados string, object e dynamic");
+Console.WriteLine();
+
+string nome = "Curso de C#";
+string titulo = "Curso de C# Essencial";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+string valor = "Isto e uma string";
+valor = "Isto e uma string alterada";
+
+object nota = 10;
+object media = 8.5;
+object materia = "fisica";
+object ativa = true;
+object letra = 'A';
+dynamic nota2 = 9;
+
+Console.WriteLine(nota);
+Console.WriteLine(media);
+Console.WriteLine(materia);
+Console.WriteLine(ativa);
+Console.WriteLine(letra);
+Console.WriteLine(nota2);
+
+Console.ReadKey();
