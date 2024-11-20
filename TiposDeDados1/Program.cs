a3c6f1f23337c2188aceb00e4f1c634ec4da262d
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-20 20:00:20 -0300

    Tipo de dados numericos

--- TiposDeDados1/Program.cs
+++ TiposDeDados1/Program.cs
@@ -7,10 +7,15 @@ int valor3 = 2147483647;
 uint valor4 = 2147483689;
 long valor5 = -2147483647;
 
+int var1 = 12, var2 = 33, var3 = 44;
+
 Console.WriteLine(valor1);
 Console.WriteLine(valor2);
 Console.WriteLine(valor3);
 Console.WriteLine(valor4);
 Console.WriteLine(valor5);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
 
 Console.ReadKey();
