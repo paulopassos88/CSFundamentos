fc2fc2abf0512d3e62cb6d2b4eb7dae91809b995
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-21 12:44:48 -0300

    Entrada de dados

---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Entrada de dados\n");
+
+Console.WriteLine("\nIforne o seu nome");
+string nome = Console.ReadLine();
+
+Console.WriteLine("\nIforne sua idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("O seu nome e " + nome);
+
+Console.WriteLine($"Sua idade e {idade}");
+
+Console.ReadKey();
