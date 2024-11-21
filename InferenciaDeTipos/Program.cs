c1357158f93f8fe265cba535a3f3a95baf42c6fc
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-21 13:37:36 -0300

    Inferencia de Tipos (var)

---
+++ InferenciaDeTipos/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Inferencia de Tipos (var) \n");
+
+var idade = 36;
+var nome = "Paulo";
+var salario = 5000.000;
+
+Console.WriteLine($"{nome} tem {idade} anos e ganha {salario.ToString("c")}");
