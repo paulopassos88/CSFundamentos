de335a30f416826485ca7680a0b456b0d6158097
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-21 10:39:16 -0300

    Conversao de tipos

---
+++ ConversaoTipos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Conversao de tipos\n");
+
+// conversao implicitas
+int valorInt = 100;
+double valorDouble = valorInt;
+
+//conversao explicitas
+double valor1 = 12.456;
+int valor2 = (int)valor1;
+
+int valor3 = 10;
+int valor4 = 4;
+
+float result = (float) valor3 / valor4;
+
+Console.WriteLine(result);
+Console.WriteLine(valorDouble);
+Console.WriteLine(valor2);
+
