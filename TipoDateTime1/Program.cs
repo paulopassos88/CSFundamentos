4c319cdc94f77ef92a81b88904e54471180ae670
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-21 08:50:57 -0300

    Tipo de dado DateTime

---
+++ TipoDateTime1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Struct DateTime\n");
+
+DateTime dataAtual = DateTime.Now;
+DateTime dataHoje = new DateTime(2024, 11, 20);
+DateTime dataHoraHoje = new DateTime(2024, 11, 20, 21, 10, 30);
+
+Console.WriteLine(dataAtual);
+Console.WriteLine(dataHoje);
+Console.WriteLine(dataHoraHoje);
+
+//extrair informacoes da data atual
+
+Console.WriteLine(dataAtual.Year);
+Console.WriteLine(dataAtual.Month);
+Console.WriteLine(dataAtual.Day);
+Console.WriteLine(dataAtual.Hour);
+Console.WriteLine(dataAtual.Minute);
+Console.WriteLine(dataAtual.Second);
+Console.WriteLine(dataAtual.Second);
+
+Console.WriteLine(dataAtual.AddDays(30));
+
+Console.ReadKey();
