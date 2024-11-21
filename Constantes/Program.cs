6f51d2379d50dcc91f1bf967f8545dcf25825943
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-21 13:53:54 -0300

    Constantes

---
+++ Constantes/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Constantes");
+
+double raio, perimetro, area;
+
+const double PI = 3.14;
+
+Console.Write("Informe o raio do circulo : ");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2 * PI * raio;
+area = PI * raio * raio;
+
+Console.WriteLine($"Perimetro = {perimetro}");
+Console.WriteLine($"Area = {area}");
