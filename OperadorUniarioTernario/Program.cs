39374c7a4b6bef0c59e162e0ff9ab2bbfa001c97
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2024-11-22 12:14:10 -0300

    Operador Uniario e Ternario

---
+++ OperadorUniarioTernario/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Operador Uniario e Ternario\n");
+
+//int positivo = 1;
+//int result;
+//result = +positivo;
+//Console.WriteLine(result);
+
+//Console.WriteLine("Inform o numero: \n");
+//var numero = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"o negativo de {numero} e {-numero}");
+
+Console.WriteLine("Inform a temperatura: \n");
+var temp = Convert.ToDouble(Console.ReadLine());
+
+var result = temp > 27 ? "Quente" : "Normal";
+
+Console.WriteLine($"o tempo esta {result}");
+
+
